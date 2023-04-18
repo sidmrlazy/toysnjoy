2bed9a9787f862f45b6c714d4f591aea432b92e9
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-04-18 14:54:04 +0530

    modified:   about/section-1.php

--- about/section-1.php
+++ about/section-1.php
@@ -1,3 +1,3 @@
 <div class="about-section-1">
-    <img src="assets/images/banners/about-banner.png" alt="">
+    <img src="assets/images/banners/about-banner.png" alt="About Toys N Joy Play School">
 </div>
